41249a8b82ab4b2b2e78b4e251813591fa9a5759
Igor Moochnick <igor.moochnick@gmail.com>
2014-03-08 13:38:34 -0500

    Fixed commit build script

--- commit_build.sh
+++ commit_build.sh
@@ -3,5 +3,5 @@ bundle --version || gem install bundler --no-ri --no-rdoc
 bundle install
 rm -f *.gem
 bundle exec rake spec || exit $?
-bundle exec gem build pipeline-generator.gemspec || exit $?
+bundle exec gem build jenkins_pipeline_builder || exit $?
 bundle exec gem bump
