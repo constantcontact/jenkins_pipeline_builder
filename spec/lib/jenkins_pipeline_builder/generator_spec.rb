b59d0279ea9ef29e83509a1822b1427e06582ab2
Gaulin, Brendan <bgaulin@constantcontact.com>
2014-06-30 13:41:09 -0400

    Blank Gen Spec

---
+++ spec/lib/jenkins_pipeline_builder/generator_spec.rb
@@ -0,0 +1,141 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+
+describe '#initialize' do
+
+end
+
+describe '#debug=' do
+
+end
+
+describe '#client' do
+
+end
+
+describe '#view' do
+
+end
+
+describe '#load_collection_from_path' do
+
+end
+
+describe '#load_job_collection' do
+
+end
+
+describe '#get_item' do
+
+end
+
+describe '#load_extensions' do
+
+end
+
+describe '#match_extension_versions' do
+
+end
+
+describe '#newest_compatible' do
+
+end
+
+describe '#load_template' do
+
+end
+
+describe '#download_yaml' do
+
+end
+
+describe '#load_remote_yaml' do
+
+end
+
+describe '#load_templates' do
+
+end
+
+describe '#cleanup_temp_remote' do
+
+end
+
+describe '#list_plugins' do
+
+end
+
+describe '#prepare_jobs' do
+
+end
+
+describe '#process_views' do
+
+end
+
+describe '#process_jobs' do
+
+end
+
+describe '#resolve_project' do
+
+end
+
+describe '#resolve_job_by_name' do
+
+end
+
+describe '#projects' do
+
+end
+
+describe '#jobs' do
+
+end
+
+describe '#publish_project' do
+
+end
+
+describe '#publish_jobs' do
+
+end
+
+describe '#bootstrap' do
+
+end
+
+describe '#pull_request' do
+  
+end
+
+describe '#dump' do
+
+end
+
+describe '#create_or_update' do
+
+end
+
+describe '#compile_job_to_xml' do
+
+end
+
+describe '#adjust_multi_project' do
+
+end
+
+describe '#compile_freestyle_job_to_xml' do
+
+end
+
+describe '#add_job_dsl' do
+
+end
+
+describe '#generate_job_dsl_body' do
+
+end
+
+describe '#build_job_dsl' do
+
+end
