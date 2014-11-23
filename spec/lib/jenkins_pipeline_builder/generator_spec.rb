f836ac74e5b6b2f6bef74cb762a2cc39ed078d5b
Anthony Gargiulo <agargiulo@constantcontact.com>
2014-11-22 19:03:20 -0500

    file mode

--- spec/lib/jenkins_pipeline_builder/generator_spec.rb
+++ spec/lib/jenkins_pipeline_builder/generator_spec.rb
@@ -214,4 +214,21 @@ describe JenkinsPipelineBuilder::Generator do
       File.delete("#{job_name}.xml")
     end
   end
+
+  describe '#file_mode' do
+    before :each do
+      allow(JenkinsPipelineBuilder.client).to receive(:plugin).and_return double(
+        list_installed: { 'description' => '20.0', 'git' => '20.0' })
+    end
+    it 'generates xml and saves to disk without sending jobs to the server' do
+      job_name = 'TemplatePipeline'
+      path = File.expand_path('../fixtures/generator_tests/template_pipeline', __FILE__)
+      stub_request(:get, 'http://username:password@127.0.0.1:8080/api/json')
+        .to_return(status: 200, body: '', headers: {})
+      errors = @generator.file(path, job_name)
+      expect(errors.empty?).to be true
+      expect(File.exist?("out/xml/#{job_name}-10.xml")).to be true
+      expect(File.exist?("out/xml/#{job_name}-11.xml")).to be true
+    end
+  end
 end
