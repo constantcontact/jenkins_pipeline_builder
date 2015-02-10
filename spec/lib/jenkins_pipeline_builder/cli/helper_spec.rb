c87e3d422b8467ca5ec7cd2fe4890c17d51f49ab
Anthony Gargiulo <agargiulo@constantcontact.com>
2015-02-10 14:54:06 -0500

    testing server cli arg

---
+++ spec/lib/jenkins_pipeline_builder/cli/helper_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe JenkinsPipelineBuilder::CLI::Helper do
+
+  let(:generator) do
+    instance_double(
+      JenkinsPipelineBuilder::Generator,
+      :debug= => true
+    )
+  end
+
+  let(:options) do
+    {
+      username: 'username',
+      password: 'password'
+    }
+  end
+
+  let(:expected_options) do
+    {
+      username: 'username',
+      password: 'password'
+    }
+  end
+
+  before(:each) do
+    expect(JenkinsPipelineBuilder).to receive(:credentials=).with(expected_options)
+    expect(JenkinsPipelineBuilder).to receive(:generator).and_return(generator)
+  end
+
+  it 'should handle server being an ip' do
+    options[:server] = '127.0.0.1'
+    expected_options[:server_ip] = '127.0.0.1'
+    described_class.setup(options)
+  end
+
+  it 'should handle server arg being a url' do
+    options[:server] = 'https://localhost.localdomain'
+    expected_options[:server_url] = 'https://localhost.localdomain'
+    described_class.setup(options)
+  end
+end
