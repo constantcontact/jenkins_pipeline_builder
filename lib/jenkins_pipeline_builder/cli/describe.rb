b89f8cc0680e30e39a1f2cebf370f56c3557568a
Will Paul <wpaul@constantcontact.com>
2016-10-06 15:50:43 -0400

    Make CLI describe method nil safe
    As is an empty extension set will throw an error whenever describe is
    run.

--- lib/jenkins_pipeline_builder/cli/describe.rb
+++ lib/jenkins_pipeline_builder/cli/describe.rb
@@ -24,7 +24,7 @@ module JenkinsPipelineBuilder
   module CLI
     entries = JenkinsPipelineBuilder.registry.entries.keys
     entries << :job_attributes
-    entries.each do |entry|
+    entries.try(:each) do |entry|
       klass_name = entry.to_s.classify
       # rubocop:disable Style/AccessModifierIndentation
       klass = Class.new(Thor) do
@@ -34,7 +34,7 @@ module JenkinsPipelineBuilder
                        JenkinsPipelineBuilder.registry.registry[:job][entry]
                      end
 
-        extensions.each do |key, extset|
+        extensions.try(:each) do |key, extset|
           # TODO: don't just take the first
           ext = extset.extensions.first
           desc key, "Details for #{ext.name}"
@@ -52,10 +52,12 @@ module JenkinsPipelineBuilder
       # rubocop:enable Style/AccessModifierIndentation
       Module.const_set(klass_name, klass)
     end
+
     class Describe < Thor
       entries = JenkinsPipelineBuilder.registry.entries.keys
       entries << :job_attributes
-      entries.each do |entry, _path|
+
+      entries.try(:each) do |entry, _path|
         klass_name = entry.to_s.classify
         singular_model = entry.to_s.singularize
 
